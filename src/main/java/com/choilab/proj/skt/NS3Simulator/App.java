6d2c5e6833418349bd15be4a23941f3f51f40102
lleejong <lleejong@gmail.com>
2016-12-12 01:58:51 +0900

    Initial Commit

---
+++ src/main/java/com/choilab/proj/skt/NS3Simulator/App.java
@@ -0,0 +1,13 @@
+package com.choilab.proj.skt.NS3Simulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
