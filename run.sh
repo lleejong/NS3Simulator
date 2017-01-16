c2c16500cd37e9f583439e835aa6dc5a95d182dc
lleejong <lleejong@gmail.com>
2017-01-16 15:47:09 +0900

    패키지명 변경

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 mvn install && mvn compile && mvn package
-java -cp /target/NS3Simulator-0.0.1-SNAPSHOT.jar com.choilab.proj.skt.Executor
+java -cp /target/NS3Simulator-0.0.1-SNAPSHOT.jar proj.skt.Executor
